ab48f7c62cf08acc568f7fc8e0e40ae005e313c7
Habeeb <amobeeb1net@gmail.com>
2024-01-05 19:50:46 +0100

    fixed

--- app/Actions/Admin/LoginAction.php
+++ app/Actions/Admin/LoginAction.php
@@ -9,17 +9,19 @@ class LoginAction
     public function execute(array $request)
     {
         $admin = Admin::where('email', $request['email'])->first();
-        $verifyPassword = Hash::check($request['password'], $admin->password);
-        if($verifyPassword){
-            $admin->tokens()->delete();
-            $token = $admin->createToken($admin->email);
-            return [ 
-                'name' => $admin->name,
-                'email' => $admin->email,
-                'token' => $token->plainTextToken
-            ];
+        if(!empty($admin)){
+            $verifyPassword = Hash::check($request['password'], $admin->password);
+            if($verifyPassword){
+                $admin->tokens()->delete();
+                $token = $admin->createToken($admin->email);
+                return [
+                    'name' => $admin->name,
+                    'email' => $admin->email,
+                    'token' => $token->plainTextToken
+                ];
+            }
+            return false;
         }
-
         return false;
 
     }
